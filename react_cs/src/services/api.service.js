860221a99cf381a1f55730faadf489e599ab8529
knatarasan <kannappan.natarasan@gmail.com>
2022-11-22 06:36:23 +0530

    React Front  end works with Basic Auth, Backend authenticates with userid password

--- react_cs/src/services/api.service.js
+++ react_cs/src/services/api.service.js
@@ -1,9 +1,13 @@
 export async function createGraph(data) {
     data = {"graph":data}
     console.log('data ',data)
+    const username= 'admin'
+    const password= 'admin'
     const requestOptions = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json',
+        'Authorization': 'Basic ' + btoa(`${username}:${password}`) },
+        
         body: JSON.stringify(data)
     };
     
