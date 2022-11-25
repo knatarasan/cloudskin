f70ee216aed00c394124c344d2637e2af84a4583
iniyan-kannappan <iniyan.kannappan@gmail.com>
2022-11-24 17:48:57 -0800

    Added user type and api comment

--- react_cs/src/DndFlow.js
+++ react_cs/src/DndFlow.js
@@ -44,6 +44,7 @@ const DnDFlow = () => {
 
   const onSave = useCallback(() => {
     console.log("graphId", graphId,graph)
+    // fetch call is made with data object , but react takes care adding owner_id: 2
     if (graphId) {
       console.log("graph exists");
     } else if (reactFlowInstance) {
