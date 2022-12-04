a567d65d556b78574c74bed70589404f6aa918af
iniyan-kannappan <iniyan.kannappan@gmail.com>
2022-12-03 22:02:32 -0800

    1. Exposed API for create EC2. 2. Implemented API and tested until instance id created and saved in database.

--- api/django_api/api/models.py
+++ api/django_api/api/models.py
@@ -3,10 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+class EC2(models.Model):
+    owner = models.ForeignKey(
+        'auth.User', related_name='ec2', on_delete=models.CASCADE # When deleted, the ec2 instances created by the user should be terminated
+    )
+    ec2_instance_id = models.TextField()
 
 class Graph(models.Model):
     owner = models.ForeignKey(
-        'auth.User', related_name='graph', on_delete=models.CASCADE)
+        'auth.User', related_name='graph', on_delete=models.CASCADE
+    )
     graph = models.JSONField()
 
     # def save(self, *args, **kwargs):
