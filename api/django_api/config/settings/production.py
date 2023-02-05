e4b2158b3f62ace482f372540820cd5e4c9d2e37
Sathis <sathiscode@users.noreply.github.com>
2023-02-05 23:19:15 +0530

    JWT settings to support only for https in production

--- api/django_api/config/settings/production.py
+++ api/django_api/config/settings/production.py
@@ -16,6 +16,11 @@ SIMPLE_JWT["SIGNING_KEY"] = env("CS_JWT_SIGNING_KEY")
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CS_DB_CONN_MAX_AGE", default=60)  # type: ignore
 
 
+#################### dj-rest-auth start ####################
+JWT_AUTH_SECURE = True  # Cookies will be sent only with https scheme
+#################### dj-rest-auth end ####################
+
+
 # LOGGING
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#logging
