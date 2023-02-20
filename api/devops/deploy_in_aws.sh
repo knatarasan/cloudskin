d50da70c4470894de03ad9234eb1fc0682637ba8
Kannappan Natarasan <kannappan_natarasan@yahoo.com>
2023-02-20 11:30:41 -0800

    With deployment updates

--- api/devops/deploy_in_aws.sh
+++ api/devops/deploy_in_aws.sh
@@ -1,4 +1,4 @@
-ssh -i ~/.ssh/cloudskin_key.pem ec2-user@ec2-54-193-172-190.us-west-1.compute.amazonaws.com
+ssh -i ~/.ssh/cloudskin_key.pem ec2-user@ec2-54-183-97-140.us-west-1.compute.amazonaws.com
 
 
 #1 SecurityGroup Inbound rules Any IP through HTTP
@@ -6,15 +6,20 @@ ssh -i ~/.ssh/cloudskin_key.pem ec2-user@ec2-54-193-172-190.us-west-1.compute.am
 #2 install postgresql
 
 # Used this link: https://blog.milliondollarserver.com/2018/09/install-postgresql-96-on-amazon-linux-2.html
-amazon-linux-extras install postgresql9.6
+sudo amazon-linux-extras install postgresql9.6
 sudo yum install postgresql-server.x86_64
 sudo systemctl enable postgresql.service
-sudo /usr/bin/postgresql-setup --initdb
+sudo /usr/bin/postgresql-setup initdb
 sudo systemctl start postgresql
 
 # manual
 sudo su - postgres
 psql postgres
+
+# create user and database
+CREATE DATABASE cs_db;
+CREATE ROLE cs WITH LOGIN PASSWORD '<password>';
+GRANT ALL PRIVILEGES ON DATABASE cs_db TO cs;
 
 #manual
 sudo vim /var/lib/pgsql/data/pg_hba.conf
@@ -23,7 +28,10 @@ sudo vim /var/lib/pgsql/data/pg_hba.conf
   # IPv4 local connections:
   host    all             all             127.0.0.1/32            md5
 
+sudo systemctl restart postgresql
+
 # To check login: psql postgresql://cs:cs@127.0.0.1:5432/cs_db
+
 
 sudo yum install -y git
 # git cone
@@ -33,14 +41,27 @@ python3 -m venv ~/.venv
 source ~/.venv/bin/activate
 
 echo 'AWS_TEST_MODE=False' > django_api/.env
-mkdir cloudskin/api/log
 
-# How to uninstall postgres
-#sudo yum remove postgresql postgresql-server
-#sudo rm -rf /var/lib/psql
+#make sure 
+# cat .env
+# AWS_TEST_MODE=False
+# CS_DATABASE_URL=postgres://cs:cs@localhost:5432/cs_db
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
 
 # For testing make sure the port number 0:8000
 python manage.py runserver 0:8000
+http://ec2-54-183-97-140.us-west-1.compute.amazonaws.com:8000/api/v1/
+
+
+# How to uninstall postgres
+# sudo yum remove postgresql postgresql-server
+# sudo rm -rf /var/lib/psql
+
+# Done until this
+
 
 cd /home/ec2-user/cloudskin/api/django_api
 gunicorn --bind localhost:8000 django_api.wsgi:application
@@ -56,13 +77,57 @@ sudo systemctl enable --now gunicorn.service
 # update proxy in package.json file to point ec2 instance  Eg: "proxy": "http://54.193.172.190/:8000",
 
 npm run build
-scp -i ~/.ssh/cloudskin_key.pem -r ~/workspace/cloudskin/react_cs/build/* ec2-user@ec2-54-193-172-190.us-west-1.compute.amazonaws.com:/tmp/build
-
-#At remote machine
 sudo rm -rf /var/www/build/*
 sudo rm -rf /tmp/build/*
 
-sudo scp -r /tmp/build/* /var/www/build/
+# make sure you have updated .env.production.local
+# https://create-react-app.dev/docs/adding-custom-environment-variables/
+
+scp -i ~/.ssh/cloudskin_key.pem -r ~/workspace/cloudskin/react_cs/build/* ec2-user@ec2-54-183-97-140.us-west-1.compute.amazonaws.com:/tmp/build
+sudo cp -r /tmp/build/* /var/www/build/
+
+#At remote machine
+sudo amazon-linux-extras install -y nginx1
+
+#update conf file of nginx
+sudo vim /etc/nginx/nginx.conf
+# Update webroot to /var/www/
+
+#Configure following stuf in nginx.conf
+'
+server {
+        listen       80;
+        listen       [::]:80;
+        server_name  ec2-54-183-97-140.us-west-1.compute.amazonaws.com;
+        root         /var/www/build;
+        location  /   {
+                try_files $uri /index.html;
+        }
+        location /api {
+                proxy_pass http://localhost:8000;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
+        # Load configuration files for the default server block.
+        include /etc/nginx/default.d/*.conf;
+
+        error_page 404 /404.html;
+        location = /404.html {
+        }
+
+        error_page 500 502 503 504 /50x.html;
+        location = /50x.html {
+        }
+    }
+'
+
+sudo systemctl restart nginx
+
+sudo rm -rf /var/www/build/*
+sudo rm -rf /tmp/build/*
+
+sudo cp -r /tmp/build/* /var/www/build/
 
 sudo service nginx stop
 sudo service nginx start
