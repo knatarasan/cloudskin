c635227a6d47b0bcfb30ab0fb271eaf0b9527a77
Kannappan Natarasan <kannappan_natarasan@yahoo.com>
2023-07-06 17:02:38 -0700

    split github actions

--- api/devops/deploy_in_aws_script.sh
+++ api/devops/deploy_in_aws_script.sh
@@ -22,7 +22,7 @@ sudo rm -rf /tmp/build/*
 
 cd cloudskin/react_cs
 npm run build
-scp -i ~/Downloads/react-deploy.pem -r ~/workspace/cloudskin/react_cs/build/* ec2-user@ec2-54-193-41-186.us-west-1.compute.amazonaws.com:/home/ec2-user/
+scp -i ~/Downloads/react-deploy.pem -r ~/workspace/cloudskin/react_cs/build/* ec2-user@ec2-54-193-41-186.us-west-1.compute.amazonaws.com:/tmp/build/
 # scp -r -i ~/.ssh/cloudskin_key.pem build/* ec2-user@ec2-54-183-97-140.us-west-1.compute.amazonaws.com:/var/www/build/
 
 # @prod machine
